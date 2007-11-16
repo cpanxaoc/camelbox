782e79cb381446a877e1ea07a04ab7dd73d0fd18
elspicyjack <devnull@localhost>
2007-11-16 17:52:21 +0000

    - added a diagram showing which packages will contain what

---
+++ docs/Camelbox_Packaging.mm
@@ -0,0 +1,36 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1195234086569" ID="Freemind_Link_1614576885" MODIFIED="1195234097601" TEXT="Camelbox Packages">
+<node CREATED="1195234946723" ID="Freemind_Link_1419940786" MODIFIED="1195235201997" POSITION="right" TEXT="camelbox-&lt;SERIAL&gt;-bin">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1195234958193" ID="Freemind_Link_1243357300" MODIFIED="1195234963604" TEXT="Perl"/>
+<node CREATED="1195235010160" ID="Freemind_Link_1541286772" MODIFIED="1195235070979" TEXT="libglib/Glib"/>
+<node CREATED="1195235015280" ID="Freemind_Link_186967255" MODIFIED="1195235067299" TEXT="libcairo/Cairo"/>
+<node CREATED="1195234994272" ID="Freemind_Link_219682669" MODIFIED="1195235075898" TEXT="libgtk+/Gtk2"/>
+</node>
+<node CREATED="1195235088647" ID="Freemind_Link_1462347246" MODIFIED="1195235247097" POSITION="left" TEXT="camelbox-&lt;SERIAL&gt;-dev">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1195235099519" ID="Freemind_Link_73797544" MODIFIED="1195235103850" TEXT="Header files"/>
+<node CREATED="1195235105207" ID="Freemind_Link_1040113499" MODIFIED="1195235124978" TEXT="Library archives (*.a)"/>
+</node>
+<node CREATED="1195235128806" ID="Freemind_Link_635946090" MODIFIED="1195235234146" POSITION="right" TEXT="camelbox-&lt;SERIAL&gt;-doc">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1195235141286" ID="Freemind_Link_267740200" MODIFIED="1195235144962" TEXT="HTML pages"/>
+<node CREATED="1195235145870" ID="Freemind_Link_776763325" MODIFIED="1195235149209" TEXT="man pages"/>
+<node CREATED="1195235150918" ID="Freemind_Link_335057445" MODIFIED="1195235154266" TEXT="text files"/>
+</node>
+<node CREATED="1195235284085" ID="Freemind_Link_1969719585" MODIFIED="1195235302080" POSITION="left" TEXT="camelbox-&lt;SERIAL&gt;-utils">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1195235310276" ID="Freemind_Link_379124700" MODIFIED="1195235325191" TEXT="demos, non-essential binaries (cjpeg for example)"/>
+</node>
+<node CREATED="1195235329436" FOLDED="true" ID="Freemind_Link_194443069" MODIFIED="1195235398083" POSITION="right" TEXT="camelbox-&lt;SERIAL&gt;-full">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1195235342108" ID="Freemind_Link_206756700" MODIFIED="1195235375879" TEXT="Everything!">
+<icon BUILTIN="stop"/>
+</node>
+<node CREATED="1195235347291" ID="Freemind_Link_231426658" MODIFIED="1195235380255" TEXT="Will be larger than the 20M google upload cap">
+<icon BUILTIN="stop"/>
+</node>
+</node>
+</node>
+</map>
