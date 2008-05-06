136370830fc06b5cd69fd3cdb89c18ba1be1f0cc
elspicyjack <devnull@localhost>
2008-05-06 19:11:59 +0000

    - added demo script for creating an asciio application file using pp

---
+++ scripts/asciio_pp.bat
@@ -0,0 +1,3 @@
+cls
+del C:\temp\asciio.exe
+C:\camelbox\bin\pp --gui --verbose --icon C:\camelbox\bin\wperl.exe -l libCairoPerl.dll -l libgthread-2.0-0.dll -o C:\temp\asciio.exe -l C:\camelbox\site\lib\auto\Cairo\Cairo.dll -l C:\camelbox\site\lib\auto\Glib\Glib.dll -l C:\camelbox\site\lib\auto\Gtk2\Gtk2.dll -M Win32::Console -M Tie::Hash::NamedCapture -M Gtk2 C:\camelbox\bin\asciio
