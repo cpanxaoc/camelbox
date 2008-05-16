2dd21cebe9020cb9d72d90fb4b4ade77977bb20f
elspicyjack <devnull@localhost>
2008-05-16 03:42:08 +0000

    - fixed slashes in gyroscope filelist - slimmed down the batch file due to having a filelist now

--- scripts/gyroscope_pp.bat
+++ scripts/gyroscope_pp.bat
@@ -1,5 +1,5 @@
 cls
 del C:\temp\gyroscope.par
-C:\camelbox\bin\pp @C:\temp\gyroscope_filelist.txt C:\camelbox\examples\gyroscope.pl
+C:\camelbox\bin\pp @C:\temp\camelbox-svn\scripts\gyroscope_filelist.txt C:\temp\camelbox-svn\scripts\gyroscope.pl
 REM run resulting PAR file with:
 REM perl -MPAR gyroscope.par gyroscope.pl
