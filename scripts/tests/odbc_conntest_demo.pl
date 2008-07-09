4d8a972cdfd20c4c6c70fef95ad9736e06e854c3
elspicyjack <devnull@localhost>
2008-07-09 08:47:46 +0000

    - added an ODBC connectivity test - tried to fix the existing ODBC test to work with the CSV driver manager/DSN, but no dice

--- scripts/tests/odbc_conntest_demo.pl
+++ scripts/tests/odbc_conntest_demo.pl
@@ -20,14 +20,20 @@
   my $dbh = DBI->connect("dbi:ODBC:$DSN", undef, undef, {RaiseError=>1}) 
   	or die qq($DBI::errstr);
   
+my %InfoTests = (
+	'SQL_DRIVER_NAME', 6,
+	'SQL_DRIVER_VER', 7,
+	'SQL_CURSOR_COMMIT_BEHAVIOR', 23,
+	'SQL_ALTER_TABLE', 86,
+	'SQL_ACCESSIBLE_PROCEDURES', 20,
+);
 
-  my $sth = $dbh->prepare("select * from $DSN");
-  $sth->execute;
-  my @rows = $sth->fetchrow_array;
-  foreach (@rows) {
-	  print qq(row: ) . $_ . qq(\n);
-  }
+foreach $SQLInfo (sort keys %InfoTests) {
+   $ret = 0;
+   $ret = $dbh->func($InfoTests{$SQLInfo}, GetInfo);
+   print "$SQLInfo ($InfoTests{$SQLInfo}):\t$ret\n";
+}
 
-  $dbh->disconnect;
+$dbh->disconnect;
 
-  exit 0;
+exit 0;
