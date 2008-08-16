2bbbcc420557d5569b27868ebd42e837a1befd79
elspicyjack <devnull@localhost>
2008-08-16 00:30:25 +0000

    - added -z (zero tables) option to dbdo.sh - renamed the table creation/zeroing scripts

--- tests/testfiles/dbdo.sh
+++ tests/testfiles/dbdo.sh
@@ -21,22 +21,26 @@ function show_usage () {
 	echo "Usage:"
 	echo "  -h show this help text"
 	echo "  -c create the database file (${SQLITE_DB_FILE})"
+	echo "  -z zero out the tables in ${SQLITE_DB_FILE}"
 	echo "  -d destroy the database file (${SQLITE_DB_FILE})"
 	echo
 	echo "Database options:"
-	echo "  -s SQLite (database filename: ${SQLITE_DB_FILE})"
+	echo "  -s connect via SQLite (database filename: ${SQLITE_DB_FILE})"
 	echo "  -f filename to use for the SQLite database file"
+	echo "  -m connect via MySQL client"
+	echo "  -p connect via PostgreSQL client"
 	echo
 	exit 1
 } # function show_usage ()
 
 #### begin main script ####
 # call getopts with all of the supported options
-while getopts cdf:h VARLIST
+while getopts hczdsf:mp VARLIST
 do
 	case $VARLIST in
 		c)  ACTION="create";;
         d)  ACTION="destroy";;
+        s)  ACTION="zero";;
 		f)  DB_FILENAME=$OPTARG;;
 		h)  SHOW_HELP="true";;
 	esac
@@ -48,15 +52,19 @@ if [ "x$SHOW_HELP" = "xtrue" ]; then show_usage; fi
 
 if [ "x$ACTION" = "xcreate" ]; then
 	# create the database
-	sqlite3 -line $SQLITE_DB_FILE '.read sqlite_create.sql'  
+	sqlite3 -line $SQLITE_DB_FILE '.read sqlite-create.sql'  
 	check_exit_code "create db file" $?	
 
+elif [ "x$ACTION" = "xzero" ]; then
+	# zero out the tables in the database
+	sqlite3 -line $SQLITE_DB_FILE '.read sqlite-zero_tables.sql'
+	check_exit_code "slick tables" $?	
+
 elif [ "x$ACTION" = "xdestroy" ]; then
-	# destroy the database
-	sqlite3 -line $SQLITE_DB_FILE '.read sqlite_destroy.sql'
-	check_exit_code "destroy db file" $?	
+	# remove the actual file
 	rm $SQLITE_DB_FILE
 	check_exit_code "delete db file" $?
+
 else
 	# neither -c (create) or -d (destroy) passed in
 	show_usage
