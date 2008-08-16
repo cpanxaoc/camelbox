fc7a16ae8e6004252074cade9319870cf9c3ef3f
elspicyjack <devnull@localhost>
2008-08-16 09:01:17 +0000

    - added more tables to the create and zero_tables files - added more to the banner screen in camelbox_functions.nsh

--- tests/testfiles/sqlite-create.sql
+++ tests/testfiles/sqlite-create.sql
@@ -1,15 +1,43 @@
 CREATE TABLE install_type (
-	type_num 			INTEGER,
-	type_desc 			TEXT
-); -- install_type
+	type_num 			    INTEGER,
+	type_desc 			    TEXT
+);
+-- CREATE TABLE install_type
 
 CREATE TABLE base_urls (
-	url					TEXT
-); -- base_urls
+	url					    TEXT
+); 
+-- CREATE TABLE base_urls
 
 CREATE TABLE manifest (
-	manifest_num 		PRIMARY KEY AUTOINCREMENT,
-	manifest_pkg_id		INTEGER
-); -- manifest
+	manifest_num 		    INTEGER PRIMARY KEY AUTOINCREMENT,
+	manifest_pkg_id		    INTEGER
+); 
+-- CREATE TABLE manifest
 
-.q
+CREATE TABLE groups (
+    group_id                TEXT,
+    group-desc              TEXT,
+    expanded_flag           INTEGER
+);
+-- CREATE TABLE groups
+
+CREATE TABLE groups-selections (
+    group_id                TEXT,
+    package_id              TEXT
+);
+-- CREATE TABLE groups-selections
+
+CREATE TABLE packages (
+    package_id              TEXT,
+    package_desc            TEXT
+);
+-- CREATE TABLE packages
+
+CREATE TABLE package_install_type (
+    package_id              TEXT,
+    install_type.type_num   INTEGER
+);
+-- CREATE TABLE package_install_type
+
+
--- tests/testfiles/sqlite-zero_tables.sql
+++ tests/testfiles/sqlite-zero_tables.sql
@@ -1,5 +1,7 @@
 -- drop all of the tables
 DROP TABLE IF EXISTS install_type;
+DROP TABLE IF EXISTS base_urls;
+DROP TABLE IF EXISTS manifest;
 -- clean up the database file
 -- NOTE: you can get rid of this if you end up deleting the file
 VACUUM;
